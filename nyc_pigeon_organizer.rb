3eea934ec222aeb7a0b1d9e1c3ea44ceb0929f95
Amy Nguyen <amy.nguyen730@gmail.com>
2019-09-18 20:21:24 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,12 +4,15 @@ def nyc_pigeon_organizer(data)
   data.each do |key, value|
     value.each do |new_value, names|
       names.each do |name|
+        
         if !pigeon_list[name]
           pigeon_list[name] = {}
         end
+        
         if !pigeon_list[name][key]
           pigeon_list[name][key] = []
         end
+        
         pigeon_list[name][key].push(new_value.to_s)
       end
     end
